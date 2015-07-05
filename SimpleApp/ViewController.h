10120520db74df9ba6364a56043e7dc89b06d317
MD <md@MacBook-Air-MD.Dlink>
2015-07-05 15:32:20 +0300

    Initial Commit

---
+++ SimpleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleApp
+//
+//  Created by MD on 05.07.15.
+//  Copyright (c) 2015 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleApp
+//
+//  Created by MD on 05.07.15.
+//  Copyright (c) 2015 hh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
